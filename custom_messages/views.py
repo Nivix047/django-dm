3f7bdea5664944cfe87bc3c6f03433afb7f65c8f
Daniel Xu <xudh47@gmail.com>
2023-09-25 20:47:17 -0700

    Added comments to custom_messages/views.py

--- custom_messages/views.py
+++ custom_messages/views.py
@@ -4,46 +4,56 @@ from django.db.models import Q
 from .models import CustomMessage    
 from .serializers import MessageSerializer
 
+# Endpoint to list all messages and create a new message
 class MessageListCreateView(generics.ListCreateAPIView):
-    queryset = CustomMessage.objects.all()    
+    queryset = CustomMessage.objects.all()  # All messages will be listed
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can access
 
+    # Overrides the save method to automatically set the sender to the current user
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
 
+# Endpoint to retrieve, update, and delete a specific message
 class MessageRetrieveUpdateDestroyView(mixins.RetrieveModelMixin, 
                                        mixins.DestroyModelMixin, 
                                        mixins.UpdateModelMixin, 
                                        generics.GenericAPIView):
     queryset = CustomMessage.objects.all()   
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can access
 
+    # Retrieve a specific message
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
+    # Delete a specific message and provide custom response on successful deletion
     def delete(self, request, *args, **kwargs):
-      response = self.destroy(request, *args, **kwargs)
-      if response.status_code == status.HTTP_204_NO_CONTENT:
-          return Response({"message": "Message deleteed successfully"}, status=status.HTTP_200_OK)
-      return response
+        response = self.destroy(request, *args, **kwargs)
+        if response.status_code == status.HTTP_204_NO_CONTENT:
+            return Response({"message": "Message deleted successfully"}, status=status.HTTP_200_OK)
+        return response
 
-    def patch(self, request, *args, **kwargs):  # Only PATCH allowed
+    # Partially update a specific message (only fields provided will be updated)
+    def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
+# Endpoint to list all messages sent or received by the current user
 class UserMessagesListView(generics.ListAPIView):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    # Filter messages to only include those sent or received by the current user
     def get_queryset(self):
         user = self.request.user
         return CustomMessage.objects.filter(Q(sender=user) | Q(recipient=user))
 
+# Endpoint to list all messages between the current user and another specified user
 class ConversationBetweenUsersListView(generics.ListAPIView):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    # Filter messages to only include those between the current user and the specified user
     def get_queryset(self):
         user1 = self.request.user
         user2_id = self.kwargs['user_id']
